5a88c5f34e0f49c0bae030a0a36eda78dd65f1a2
harshita-arikatla <arikatla.harshita@gmail.com>
2022-07-08 00:24:48 +0530

    adding master

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapiharshita/LearningresourcesApiHarshitaApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapiharshita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiHarshitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiHarshitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresourcesapiharshita/LearningresourcesApiHarshitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapiharshita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiHarshitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
