2d46b82e542521a05bb25c1fee99c891def43311
HARSHITA <108134552+harshita-arikatla@users.noreply.github.com>
2022-07-08 22:45:35 +0530

    Update LearningresourcesApiHarshitaApplication.java

--- src/main/java/com/tgt/rysetii/learningresourcesapiharshita/LearningresourcesApiHarshitaApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapiharshita/LearningresourcesApiHarshitaApplication.java
@@ -8,6 +8,7 @@ public class LearningresourcesApiHarshitaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesApiHarshitaApplication.class, args);
+		System.out.println("Hello world!");
 	}
 
 }
